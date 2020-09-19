c63bb8ac8cb96900080c1824a39837fd2518ab5f
billywern <billywai96@gmail.com>
2020-09-19 13:49:34 +0800

    - Added table to show list of restaurants - Added a collapse to show more details of the menu to the particular restaurant

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { Landing } from './landing-page';
+import { AvailableOrder } from './landing-page';
 import theme from './util/theme';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <Landing />
+    <AvailableOrder />
   </ThemeProvider>,
   document.querySelector('#root'),
 );
---
+++ src/services/restaurants.ts
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+export const getAvailableRestaurants = async ():Promise<GetAvailableRestaurantResponse> => {
+  const { data } = await axios.get('/restaurants')
+  return data
+}
+
+export interface GetAvailableRestaurantResponse {
+  data: GetAvailableRestaurantData[]
+}
+
+export interface GetAvailableRestaurantData {
+  restaurantId: string
+  name: string
+  operationHours: OperationHours
+  offDays: string[]
+  menu: menuItem[]
+  isAvailable: boolean
+}
+
+interface OperationHours {
+  startTime: string
+  endTime: string
+}
+
+interface menuItem {
+  menuId: string
+  name: string
+  price: number
+}
